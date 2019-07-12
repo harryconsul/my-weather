d4f90e0798fc3099dff8f314003ec8fd515edb40
miguel.guzman <miguel.guzman@dicipa.com.mx>
2019-07-12 12:10:53 -0500

    Updates

---
+++ precache-manifest.db363defd6f377dd01b4b3f5bd0da185.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d9a0215df9ad8e8f6785f096c3dddda",
+    "url": "/my-weather/index.html"
+  },
+  {
+    "revision": "823a43476af5bba6b414",
+    "url": "/my-weather/static/css/main.6cfc9808.chunk.css"
+  },
+  {
+    "revision": "cda7ee185f4ec272927e",
+    "url": "/my-weather/static/js/2.96df1c6b.chunk.js"
+  },
+  {
+    "revision": "823a43476af5bba6b414",
+    "url": "/my-weather/static/js/main.66a2e67f.chunk.js"
+  },
+  {
+    "revision": "71373542fb7dfcf4e5f5",
+    "url": "/my-weather/static/js/runtime~main.37f04531.js"
+  },
+  {
+    "revision": "1aa0732979c91acdc7b69b3d2550b528",
+    "url": "/my-weather/static/media/bg.1aa07329.jpg"
+  },
+  {
+    "revision": "1c14910cb0326222f81a87e098ccf6c9",
+    "url": "/my-weather/static/media/weathertime-webfont.1c14910c.woff2"
+  },
+  {
+    "revision": "5ed93d1d90c85d011df229802c549000",
+    "url": "/my-weather/static/media/weathertime-webfont.5ed93d1d.woff"
+  },
+  {
+    "revision": "b363442b9e9102c34641875f3dc4090c",
+    "url": "/my-weather/static/media/weathertime-webfont.b363442b.ttf"
+  }
+]);
